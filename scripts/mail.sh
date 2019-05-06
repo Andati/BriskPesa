6f56ed19e8540f6bc68a285a37332d0791c49b35
Rodgers Andati <andatirodgers@gmail.com>
2019-05-06 17:04:47 +0300

    corrected mail script

--- scripts/mail.sh
+++ scripts/mail.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-$file=../sample/build/outputs/apk/debug/*.apk
+#file=../sample/build/outputs/apk/debug/*.apk
+file=`pwd`
 
 curl -n --ssl-reqd --mail-from "bimapap2017@gmail.com" \
---mail-rcpt "andatirodgers@gmail.com" -T - --url smtp://smtp.gmail.com:587 \
---user "bimapap2017@gmail.com:bimapap123" \
-<<< echo "$file has been created. Whoohooo!"
+--mail-rcpt "andatirodgers@gmail.com" -T - --url smtps://smtp.gmail.com:465 \
+--user "bimapap2017@gmail.com:bimapap123" --insecure \
+<<<`echo "$file has been created! Hurray!!"`
 
