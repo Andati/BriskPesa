079cde28a8bb27e9c237d289b492cf8a230196f7
Rodgers Andati <andatirodgers@gmail.com>
2019-05-06 18:09:53 +0300

    list any apks

--- scripts/mail.sh
+++ scripts/mail.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-file=sample/build/outputs/apk/debug/*.apk
-
+#file=sample/build/outputs/apk/debug/*.apk
+file=`find $directory -type f -name "*.apk"`
 
 curl -n --ssl-reqd --mail-from "bimapap2017@gmail.com" \
 --mail-rcpt "andatirodgers@gmail.com" -T - --url smtps://smtp.gmail.com:465 \
 --user "bimapap2017@gmail.com:bimapap123" --insecure \
-<<<`echo "$file has been created! Hurray!!"`
+<<<`echo "$file has been created! Hurray!"`
 
