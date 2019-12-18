3fbe72666002583f87163df8d401dbf469c4e2a4
Rodgers Andati <andatirodgers@gmail.com>
2019-12-18 15:12:47 +0300

    add cicd script

---
+++ scripts/cicd.py
@@ -0,0 +1,40 @@
+import requests
+import sys
+
+GraphQL_URL = "https://api.github.com/graphql"
+
+if len(sys.argv) == 1:
+	print "pass your GitHub key to this script"
+	exit(0)
+
+headers = {"Authorization": "Bearer " + sys.argv[1]}
+
+query = """
+{
+  search(query: "repo:Andati/BriskPesa is:pr is:merged merged:>2019-12-09", type: ISSUE, last: 100) {
+    edges {
+      node {
+        ... on PullRequest {
+          url
+          title
+          bodyText
+          createdAt
+          mergedAt
+        }
+      }
+    }
+  }
+}
+"""
+
+response = requests.post(GraphQL_URL, json={'query':query}, headers=headers)
+if response.status_code == 200:
+	json_response = response.json()
+	#print json_response['data']
+	edges = json_response['data']['search']['edges']
+	for edge in edges:
+		#print edge['node']['title']
+		print edge['node']['bodyText']
+else:
+	print "Error code returned " + str(response.status_code)
+
